9c3b9d4a57300370b4a3e4cd316b904d06730b4c
Easyyy <phunguyensm@gmail.com>
2024-07-02 16:28:44 +0700

    add a text so that the bot won't make any assumptions

--- testChatbot.py
+++ testChatbot.py
@@ -21,7 +21,6 @@ local_path = "./assets/sql-for-data-analysis-advanced-techniques-for-transformin
 loader = PyPDFLoader(file_path=local_path)
 data = loader.load()  # Load the entire PDF document into memory
 first_chapter = data[11:32]  # Extract a subset of pages (from 12th to 32nd page)
-print(len(data))
 
 # Split the extracted document into smaller text chunks
 text_splitter = RecursiveCharacterTextSplitter(chunk_overlap=0, separators=["\n", "\n\n"])
@@ -41,7 +40,7 @@ llm = ChatOllama(model=local_model, temperature = 0)
 # Define a prompt template for generating alternative versions of user queries
 QUERY_PROMPT = PromptTemplate(
     input_variables=["query"],
-    template="""You are an AI language model assistant. Your task is to generate five
+    template="""You are an AI language model assistant. You must not make any assumptions. Your task is to generate five
     different versions of the given user question to retrieve relevant documents from
     a vector database. By generating multiple perspectives on the user question, your
     goal is to help the user overcome some of the limitations of the distance-based
@@ -74,7 +73,7 @@ chain = (
 )
 
 # Interactive loop to continuously process user input
-    
+
 def give_ans(question):
     return chain.invoke(question)
 
