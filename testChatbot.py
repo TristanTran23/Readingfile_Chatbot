51e46ccd341dcd597be9aca9fa6fe1f144a194fc
TristanTran23 <tranquangthinhtdn@gmail.com>
2024-07-04 17:02:53 +0700

    testing

--- testChatbot.py
+++ testChatbot.py
@@ -7,7 +7,7 @@ from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
 from langchain_community.document_loaders import PyPDFLoader, OnlinePDFLoader
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import RetrievalQA, SimpleSequentialChain
+from langchain.chains import RetrievalQA, SequentialChain, LLMChain
 from langchain.prompts import PromptTemplate, ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.chat_models import ChatOllama
@@ -15,7 +15,10 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain.retrievers import ContextualCompressionRetriever, MultiQueryRetriever
 from langchain.retrievers.document_compressors import LLMChainExtractor
 
-vector_store = Chroma(persist_directory=".//chroma_db")
+vector_store = Chroma(
+    persist_directory=".//chroma_db",
+    embedding_function=OllamaEmbeddings(model="nomic-embed-text"),
+)
 
 # Initialize a ChatOllama language model
 local_model = "llama3"
@@ -25,7 +28,7 @@ llm = ChatOllama(model=local_model, temperature = 0)
 QUERY_PROMPT = PromptTemplate(
     input_variables=["query"],
     template="""you are an AI teacher assistant. you must not make any assumptions. you must use the information given by my pdf files. 
-        you are required to find at least 3 paragraphs related to the user's question in this textbook. each paragraph must directly answer the question or give helpful information.
+        you are required to find at least 5 paragraphs related to the user's question in this textbook. each paragraph must directly answer the question or give helpful information.
         provide the answer in seperated paragraphs. Provide these alternative questions separated by newlines.
         Original question: {question}""",
 )
@@ -52,22 +55,44 @@ Question: {question}
 """
 
 # Initialize a ChatPromptTemplate from the defined template
+first_prompt = ChatPromptTemplate.from_template(
+    "Translate the following question to english:"
+    "\n\n{question}"
+)
+
+second_prompt = ChatPromptTemplate.from_template(
+    """you are an AI teacher assistant. you must not make any assumptions. you must use the information given by my pdf files.
+    you are required to find at least 5 paragraphs related to the user's question in this textbook. each paragraph must directly answer the question or give helpful information.
+    provide the answer in seperated paragraphs. Provide these alternative questions separated by newlines.
+    
+    Original question: {english_question}"""
+)
+
 prompt = ChatPromptTemplate.from_template(template)
 
-dataFiltered = vector_store.similarity_search()
-
 # Define a chain of operations to process user input
+# chain_1 = LLMChain(
+#     llm=llm, prompt=first_prompt, 
+#     output_key="english_question"
+# )
+chain_2 = RetrievalQA(retriever=retriever, prompt=second_prompt)
+
 chain = (
-    {"context": retriever, "question": RunnablePassthrough()}  # Inputs: context from retriever, question directly from user
+    {"context": compression_retriever, "question": RunnablePassthrough()}  # Inputs: context from retriever, question directly from user
     | prompt  # Pass through the prompt template
     | llm  # Process with the ChatOllama model
     | StrOutputParser()  # Parse the output into a string format
 )
 
-# Interactive loop to continuously process user input
+# overall_chain = SequentialChain(
+#     chains=[chain_1, chain],
+#     input_keys=["question"],
+#     output_keys=["answer"],
+# )
+
 
 def give_ans(question):
     return chain.invoke(question)
 
 demo = gr.Interface(fn=give_ans, inputs="text", outputs="text")
-demo.launch(share=True)
+demo.launch()
